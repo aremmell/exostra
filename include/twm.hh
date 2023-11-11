0028f5623f519b485e3bdfa9e47caaf8ee9569f8
Ryan M. Lederman <lederman@gmail.com>
2023-11-11 00:18:24 -0700

    consolidate, refactor GfxContext

--- include/twm.hh
+++ include/twm.hh
@@ -26,10 +26,152 @@
 #ifndef _TWM_H_INCLUDED
 # define _TWM_H_INCLUDED
 
-#include <gfx_driver.hh>
+# include <cstdlib>
+# include <cstdint>
+# include <cinttypes>
+# include <type_traits>
+# include <memory>
+# include <mutex>
+
+# include <Adafruit_GFX.h>
+
+// TODO: remove me
+#define TWM_COLOR_565
+
+#define TWM_ASSERT(expr) \
+    do { \
+        if (!(expr)) { \
+            Serial.printf("error: assert!!! (" #expr ") at %s:%d\n", \
+                __FILE__, __LINE__); \
+        } \
+    } while (false);
 
 namespace twm
 {
+    /** For now, the only low-level graphics interface supported is Adafruit's. */
+    using RawGfxInterface = Adafruit_GFX;
+
+    /** Use the smallest type that can contain all possible colors. */
+# if defined(TWM_COLOR_MONOCHROME) || defined(TWM_COLOR_256)
+    using Color = uint8_t; /** Color (monochrome/8-bit). */
+# elif defined(TWM_COLOR_565)
+    using Color = uint16_t; /** Color (16-bit 565 RGB) */
+# elif defined(TWM_COLOR_RGB) || defined(TWM_COLOR_ARGB)
+    using Color = uint32_t; /** Color (24-bit RGB/32-bit ARGB). */
+# else
+#  error "color mode is invalid"
+# endif
+
+    /** Coordinate in 3D space (e.g. X, Y, or Z). */
+    using Coord = int16_t;
+
+    /** Extent (e.g. width, height). */
+    using Extent = uint16_t;
+
+    /** Point in 2D space. */
+    struct Point
+    {
+        Coord x; /** X-axis value. */
+        Coord y; /** Y-axis value. */
+    };
+
+    /** Two points in 2D space (left/top, right/bottom). */
+    struct Rect
+    {
+        Coord left {};    /** X-axis value of the left edge. */
+        Coord top {};     /** Y-axis value of the top edge. */
+        Coord right {};   /** X-axis value of the right edge. */
+        Coord bottom {};  /** Y-axis value of the bottom edge. */
+
+        inline Extent width() const noexcept
+        {
+            TWM_ASSERT(right >= left);
+            return static_cast<Extent>(right - left);
+        }
+
+        inline Extent height() const noexcept
+        {
+            TWM_ASSERT(bottom >= top);
+            return static_cast<Extent>(bottom - top);
+        }
+
+        inline Point getTopLeft() const noexcept
+        {
+            return { left, top };
+        }
+
+        inline Point getBottomRight() const noexcept
+        {
+            return { right, bottom };
+        }
+
+        inline void grow(Extent px) noexcept
+        {
+            left   -= px;
+            top    -= px;
+            right  += px;
+            bottom += px;
+        }
+
+        inline void shrink(Extent px) noexcept
+        {
+            left   += px;
+            top    += px;
+            right  -= px;
+            bottom -= px;
+        }
+
+        inline bool isSquare() const noexcept
+        {
+            return width() == height();
+        }
+
+        inline bool isHorizontal() const noexcept
+        {
+            return width() > height();
+        }
+
+        inline bool isVertical() const noexcept
+        {
+            return height() > width();
+        }
+    };
+
+    template<class TImpl>
+    class GfxContext : public TImpl
+    {
+    public:
+        template<typename... TArgs>
+        GfxContext(TArgs&& ...args) : TImpl(args...) { }
+
+        virtual ~GfxContext() = default;
+
+        static std::shared_ptr<GfxContext<TImpl>>& getInstance()
+        {
+            TWM_ASSERT(_instance != nullptr);
+            return _instance;
+        }
+
+        template<typename... TArgs>
+        static std::shared_ptr<GfxContext<TImpl>> init(TArgs&& ...args)
+        {
+            static std::once_flag flag;
+            std::call_once(flag, [&]()
+            {
+                _instance.reset(new GfxContext<TImpl>(args...));
+            });
+            TWM_ASSERT(_instance != nullptr);
+            return _instance;
+        }
+
+        virtual void tearDown() { }
+
+    protected:
+        static std::shared_ptr<GfxContext<TImpl>> _instance;
+    };
+
+    template<class TImpl>
+    std::shared_ptr<GfxContext<TImpl>> GfxContext<TImpl>::_instance;
 
 } // namespace twm
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,113 @@
-#include <twm.hh>
+/***
+ * TFT capacative touch on ProS3. Pins:
+ * sda 8
+ * scl 9
+ * tcs 12
+ * dc 13
+ * rst 14
+ */
 
-int main(int argc, char** argv)
+#include <twm.hh>
+#include <SPI.h>
+#include <Adafruit_ILI9341.h>
+#include <Wire.h>
+#include <Adafruit_FT6206.h>
+#include <UMS3.h>
+#include <aremmell_um.h>
+
+// The display also uses hardware SPI, plus these pins:
+#define TFT_CS 12
+#define TFT_DC 13
+
+// If no touches are registered in this time, paint the screen
+// black as a pseudo-screensaver. In the future, save what was on
+// the screen and restore it after.
+#define TFT_TOUCH_TIMEOUT 10000
+
+// Display size
+#define TFT_WIDTH 240
+#define TFT_HEIGHT 320
+
+// Calibration data
+#define TS_MINX 0
+#define TS_MINY 0
+#define TS_MAXX 240
+#define TS_MAXY 320
+
+using namespace aremmell;
+using namespace twm;
+
+// The FT6206 uses hardware I2C (SCL/SDA)
+Adafruit_FT6206 ctp;
+UMS3 ums3;
+
+using TwmGfxCtx = GfxContext<Adafruit_ILI9341>;
+auto tft = TwmGfxCtx::init(TFT_CS, TFT_DC);
+
+static inline
+void draw_initial_screen()
 {
-    return EXIT_SUCCESS;
+  tft->fillScreen(ILI9341_BLACK);
+  tft->println("Go ahead, touch that smoke wagon.");
+}
+
+void setup(void)
+{
+  Serial.begin(115200);
+  while (!Serial) {
+    /// TODO: If you want to run without a serial cable, exit this loop
+    delay(10);
+  }
+
+  TWM_ASSERT(tft == TwmGfxCtx::getInstance());
+
+  ums3.begin();
+  tft->begin();
+
+  if (!ctp.begin(40, &Wire)) {
+    Serial.println("FT6206: error!");
+    on_fatal_error(ums3);
+  }
+
+  Serial.println("FT6206: OK");
+
+  tft->setRotation(3);
+  tft->setCursor(0, 0);
+
+  draw_initial_screen();
+}
+
+u_long last_touch = 0UL;
+bool screensaver_on = false;
+
+static inline
+void on_screensaver_gone()
+{
+  draw_initial_screen();
+}
+
+void loop()
+{
+  if (ctp.touched()) {
+    last_touch = millis();
+    if (screensaver_on) {
+      screensaver_on = false;
+      draw_initial_screen();
+    }
+    TS_Point pt = ctp.getPoint();
+
+    pt.x = map(pt.x, TS_MINX, TS_MAXX, TS_MAXX, TS_MINX);
+    pt.y = map(pt.y, TS_MINY, TS_MAXY, TS_MAXY, TS_MINY);
+
+    long tmp = pt.y;
+    pt.y = pt.x;
+    pt.x = tft->width() - tmp;
+
+    tft->drawCircle(pt.x, pt.y, 5, ILI9341_CYAN);
+  } else {
+    if (!screensaver_on && millis() - last_touch > TFT_TOUCH_TIMEOUT) {
+      tft->fillScreen(ILI9341_BLACK);
+      screensaver_on = true;
+    }
+  }
 }
